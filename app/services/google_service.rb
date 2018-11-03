8c518b85b4b33c8e9cb88b163a1ba942ea9a27d4
Mike <mikecm@gmail.com>
2018-11-03 16:33:18 -0600

    refactors google service class

--- app/services/google_service.rb
+++ app/services/google_service.rb
@@ -1,7 +1,15 @@
 class GoogleService
 	def coordinates(city, state)
-		conn = Faraday.new(url: 'https://maps.googleapis.com/')
-		response = conn.get("/maps/api/geocode/json?address=Denver+CO&key=#{ENV['GOOGLE_API_KEY']}")
-		JSON.parse(response.body, symbolize_names: true)[:results]
+		get_json("/maps/api/geocode/json?address=#{city}+#{state}&key=#{ENV['GOOGLE_API_KEY']}")[:results]
+	end
+
+	private
+
+	def conn
+		Faraday.new(url: 'https://maps.googleapis.com/')
+	end
+
+	def get_json(url)
+		JSON.parse(conn.get(url).body, symbolize_names: true)
 	end
 end
