bf138d31e34d153005f6d8d0130dfc27e3ce3b3e
Mike <mikecm@gmail.com>
2018-11-04 11:35:25 -0700

    creates migration for favorites table

--- app/controllers/api/v1/favorites_controller.rb
+++ app/controllers/api/v1/favorites_controller.rb
@@ -1,4 +1,12 @@
 class Api::V1::FavoritesController < ApplicationController
   def create
+    user = User.find_by_api_key(favorite_params[:api_key])
+    user.favorites.find_or_create_by(location: params[:location])
+  end
+
+  private
+
+  def favorite_params
+    params.permit(:location, :api_key)
   end
 end
