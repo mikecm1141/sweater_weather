83391ea0bf8088ce40f5f1b48327369ede638796
Mike <mikecm@gmail.com>
2018-11-04 11:39:47 -0700

    creates serializer for a favorite

--- app/controllers/api/v1/favorites_controller.rb
+++ app/controllers/api/v1/favorites_controller.rb
@@ -1,7 +1,8 @@
 class Api::V1::FavoritesController < ApplicationController
   def create
     user = User.find_by_api_key(favorite_params[:api_key])
-    user.favorites.find_or_create_by(location: params[:location])
+    favorite = user.favorites.find_or_create_by(location: params[:location])
+    render json: FavoriteSerializer.new(favorite), status: 201
   end
 
   private
