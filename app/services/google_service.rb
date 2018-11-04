ce0d3f307ffc34720b6f83a354ea69f7df62e5ce
Mike <mikecm@gmail.com>
2018-11-03 22:14:45 -0600

    fixes indentation in google service

--- app/services/google_service.rb
+++ app/services/google_service.rb
@@ -1,15 +1,15 @@
 class GoogleService
-	def coordinates(city, state)
-		get_json("/maps/api/geocode/json?address=#{city}+#{state}&key=#{ENV['GOOGLE_API_KEY']}")[:results]
-	end
+  def coordinates(city, state)
+    get_json("/maps/api/geocode/json?address=#{city}+#{state}&key=#{ENV['GOOGLE_API_KEY']}")[:results]
+  end
 
-	private
+  private
 
-	def conn
-		Faraday.new(url: 'https://maps.googleapis.com/')
-	end
+  def conn
+    Faraday.new(url: 'https://maps.googleapis.com/')
+  end
 
-	def get_json(url)
-		JSON.parse(conn.get(url).body, symbolize_names: true)
-	end
+  def get_json(url)
+    JSON.parse(conn.get(url).body, symbolize_names: true)
+  end
 end
