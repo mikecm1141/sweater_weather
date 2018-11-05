c218533e332507a18cac0b6f09a3a20036b9644d
Mike <mikecm@gmail.com>
2018-11-05 12:30:22 -0700

    refactors flickr service

--- app/services/flickr_service.rb
+++ app/services/flickr_service.rb
@@ -1,7 +1,15 @@
 class FlickrService
   def location_images(lat, lng)
-    conn = Faraday.new(url: 'https://api.flickr.com/')
-    response = conn.get("/services/rest/?method=flickr.photos.search&api_key=#{ENV['FLICKR_API_KEY']}&lat=#{lat}&lon=#{lng}&format=json&nojsoncallback=1&extras=url_o")
-    JSON.parse(response.body, symbolize_names: true)[:photos]
+    get_json("/services/rest/?method=flickr.photos.search&api_key=#{ENV['FLICKR_API_KEY']}&lat=#{lat}&lon=#{lng}&format=json&nojsoncallback=1&extras=url_o")
+  end
+
+  private
+
+  def conn
+    Faraday.new(url: 'https://api.flickr.com/')
+  end
+
+  def get_json(url)
+    JSON.parse(conn.get(url).body, symbolize_names: true)[:photos]
   end
 end
