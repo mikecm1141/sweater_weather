9a30a090f99246316e268d34ce5bdb48f3b2070b
Mike <mikecm@gmail.com>
2018-11-05 13:16:49 -0700

    creates request spec for user deleting a favorite location

---
+++ spec/requests/user_deletes_favorite_api_spec.rb
@@ -0,0 +1,30 @@
+require 'rails_helper'
+
+describe 'User deletes a favorite location' do
+  context 'via DELETE /api/v1/favorites' do
+    it 'removes that favorite' do
+      user = create(:user)
+      user.favorites.create(location: 'Denver, CO')
+      user.favorites.create(location: 'Honolulu, HI')
+
+      payload = {
+        api_key:  user.api_key,
+        location: 'Denver, CO'
+      }
+
+      delete '/api/v1/favorites', params: payload
+
+      result = JSON.parse(response.body, symbolize_names: true)
+
+      expect(response).to be_successful
+      expect(result).to have_key(:data)
+      expect(result[:data]).to be_a(Array)
+      expect(result[:data].size).to eq(1)
+      expect(result[:data][0]).to have_key(:meta)
+      expect(result[:data][0][:meta]).to have_key(:data)
+      expect(result[:data][0][:meta][:data]).to have_key(:id)
+      expect(result[:data][0][:meta][:data]).to have_key(:attributes)
+      expect(result[:data][0][:meta][:data][:attributes]).to have_key(:currently)
+    end
+  end
+end
